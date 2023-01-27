6618127c7b41babb36c1e45316c3fdc7fd8c9f0a
Sergey Peshkov <zloi.g1rz@gmail.com>
2023-01-27 11:53:16 +0800

    прогрессбар

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
     slider();
     let selector = new SelectorItem('.selector');
+
+    let progressbar = new ProgressbarItem('.progressbar-wrap');
     
 });
 
@@ -64,3 +66,38 @@ class SelectorItem {
     }
 }
 
+class ProgressbarItem {
+
+    constructor(selector) {
+        this.selector = selector;
+        this.progressbarItem = document.querySelector(selector);
+
+        if (this.progressbarItem) {
+            this.btn = this.progressbarItem.querySelector('.progressbar-btn');
+            this.line = this.progressbarItem.querySelector('.progressbar__inner');
+            this.addWidth = +this.btn.getAttribute('data-add');
+        } else {
+            console.error(`Селектор ${selector} не найден!`);
+            return
+        }
+
+        this.events()
+    }
+
+    events() {
+        const $progressbarBtn = document.querySelector('.progressbar-btn');
+        const $prograssbarLine = document.querySelector('.progressbar__inner');
+        this.btn.addEventListener('click', e => {
+            
+            let width = this.line.style.width;
+            width = +width.replace(/%/g, '');
+            let newWidth = width + this.addWidth;
+
+            newWidth = newWidth > 100 ? 100 : newWidth;
+
+            this.line.style.width = newWidth + '%';
+            this.line.textContent = newWidth + ' %';
+        });
+    }
+    
+}
